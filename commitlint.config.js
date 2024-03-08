0ce7b9ecda54be749a9bc86ad6261f3374fa1fd1
lijing <51325lj@sina.com>
2024-03-08 11:16:50 +0800

    test: 新增提交规范测试

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,25 @@
-module.exports = {extends: ['@commitlint/config-conventional']};
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+  "type-enum": [
+    2,
+    "always",
+    [
+      "feat",
+      "fix",
+      "perf",
+      "style",
+      "docs",
+      "test",
+      "refactor",
+      "build",
+      "ci",
+      "chore",
+      "revert",
+      "wip",
+      "workflow",
+      "types",
+      "release"
+    ]
+  ]
+}};
