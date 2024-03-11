64d6db1100f1beffa62043e55756dbe45797220e
lijing <51325lj@sina.com>
2024-03-11 13:47:58 +0800

    ci: test

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,15 +1,15 @@
 module.exports = {
   plugins: {
     // postcss-pxtorem 插件的版本需要 >= 5.0.0
-    "postcss-pxtorem": {
-      rootValue({ file }) {
-        return file.indexOf("vant") !== -1 ? 37.5 : 75;
-      },
-      propList: ["*"]
-    }
-    // 'postcss-pxtorem': {
-    //   rootValue: 37.5,
-    //   propList: ['*'],
-    // },
+    // "postcss-pxtorem": {
+    //   rootValue({ file }) {
+    //     return file.indexOf("vant") !== -1 ? 37.5 : 75;
+    //   },
+    //   propList: ["*"]
+    // }
+    'postcss-pxtorem': {
+      rootValue: 37.5,
+      propList: ['*'],
+    },
   }
 };
